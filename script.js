ec7235efc597ae7821757f7ddbab1db398394e6d
gerald0serprof <gerald.lima.587@alumni.serprofes.es>
2026-04-17 21:52:23 +0200

    Added exercise Semana_0 from Jorge O. AI Lab

---
+++ script.js
@@ -0,0 +1,14 @@
+let miBoton = document.getElementById("theme-toggle");
+let elCuerpo = document.body;
+
+miBoton.addEventListener("click", function(){
+    elCuerpo.classList.toggle("bright-mode");
+    miBoton.textContent = "Dark Mode";
+/*
+    if (elCuerpo.classList.contains("bright-mode")){
+        miBoton.textContent = "🌙Dark Mode";
+    } else {
+        miBoton.textContent = "☀️Bright Mode";
+    }
+*/
+});
