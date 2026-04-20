1739bd94bd5525e7c8cd0d30de0ed3179edeab3a
gerald0serprof <gerald.lima.587@alumni.serprofes.es>
2026-04-20 21:44:01 +0200

    added a button modified stylesheet

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ let elCuerpo = document.body;
 miBoton.addEventListener("click", function(){
     elCuerpo.classList.toggle("bright-mode");
     miBoton.textContent = "Dark Mode";
-/*
+
     if (elCuerpo.classList.contains("bright-mode")){
         miBoton.textContent = "🌙Dark Mode";
     } else {
         miBoton.textContent = "☀️Bright Mode";
     }
-*/
+
 });
