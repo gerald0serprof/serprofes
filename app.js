7476d390c2414b68fe5aeeeeba6063c9e336ef59
gerald0serprof <gerald.lima.587@alumni.serprofes.es>
2026-05-14 21:55:57 +0200

    Modify section, Added bubles,sr-only and some styles

--- app.js
+++ app.js
@@ -11,3 +11,23 @@ function enviarPrompt(event) {
     }
 
 }
+
+function animacion(elemento) {
+    const logo = document.getElementById("logo");
+
+    logo.addEventListener("pointerover", () => {
+        logo.style.transform = "scale(1.1)";
+        setTimeout(() => {
+            elemento.style.backgroundColor = "green";
+        }, 500);
+    });
+
+    logo.addEventListener("pointerout", () => {
+        logo.style.transform = "scale(1)";
+        setTimeout(() => {
+            elemento.style.backgroundColor = "transparent";
+        }, 500);
+    });
+
+    
+}
